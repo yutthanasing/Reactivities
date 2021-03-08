f5277a1d629a05c86aa8b99e39d945f793433903
yutthana <yutthana@iconext.co.th>
2021-03-08 14:28:43 +0700

    End of Section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("Api/[Controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
